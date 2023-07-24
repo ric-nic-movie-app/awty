e0bf4f6f1656c19503e3f8cdbc31e34101e963bc
Nicholas Rowley <nicholas.ryan.rowley@gmail.com>
2023-07-24 15:36:01 -0500

    prepwork complete-ish

---
+++ js/movie-app.js
@@ -0,0 +1,15 @@
+//AJAX REQUESTS/////////////////////
+//AJAX Requests include error handling and loading animation is displayed during request.
+//Buttons are disabled during the request, and the user is given feedback on the status of the requests
+
+//DOM Manip/////////////////////////
+//HTML is generated with appropriate CSS styles and animations, and provides an engaging and intuitive user experience.
+
+//Movie Management////////////////////
+//Editing existing movies is implemented, allowing users to edit their own ratings/reviews of movies. Movie management includes sorting movies by rating, title, or genre, and searching by rating, title or genre
+
+//UX/UI///////////////////////
+//Layout and design of application is consistent, intuitive, and well-designed. User feedback is considered and implemented to improve usability.
+
+//Code Quality////////////////////
+//Code is optimized for performance and follows accessibility and security best practices. Code is well-documented and includes tests for functionality and edge cases.
